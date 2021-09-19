679ccbbf00916e5bfaf584daa93c145ba56a98ee
FabricioSC123 <89610142+FabricioSC123@users.noreply.github.com>
2021-09-19 15:23:58 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,6 +18,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/spark_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_surya-user \
-    spark_surya-userdebug \
-    spark_surya-eng
+    spark_surya-userdebug
